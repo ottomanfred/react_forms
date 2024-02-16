d273a5055c61961ce20032a0ffc2566c3ba2932f
otto18 <otto@ceramic.global>
2024-02-16 14:42:14 -0500

    remove console logs

--- src/CredentialsForm.jsx
+++ src/CredentialsForm.jsx
@@ -28,7 +28,6 @@ export default function CredentialsForm() {
         }
       );
       const parsedResponse = await response.json();
-      console.log(parsedResponse);
       setApiToken(parsedResponse.token);
     } catch (err) {
       console.log(err);
